0fb3ae7b7a6bfde30dd36756a91af175b29adcdb
Nubia Goncalves <nu.ragagnin91@gmail.com>
2024-05-10 11:51:39 -0300

    FEAT: ADD RESOLUTIONFOR BEECROWD EXERCISE 1009-SALARY WITH BONUS.

---
+++ 1009-tarefa-salario-com-bonus.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 1009_salario_com_bonus
+{
+    internal class 1009_salario_com_bonus
+    {
+        public 1009_salario_com_bonus()
+{
+
+}
+        public void solucaoSalarioComBonus()
+        {
+            string nomeVendedor = Console.ReadLine();
+            double salarioFixo = double.Parse(Console.ReadLine());
+            double totalVendasMes = double.Parse(Console.ReadLine());
+
+            double totalReceberMes = salarioFixo + 0.15 * totalVendasMes;
+
+
+            Console.WriteLine("TOTAL = R$ " + totalReceberMes.ToString("0.00"));
+
+        }
+    }
+}
