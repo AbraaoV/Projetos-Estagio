27dd66842d0ebfb68c226331047e02acc81a99b4
Karoline Marques de Miranda <marqueskarol682@gmail.com>
2024-05-10 11:15:36 -0300

    Exercicio 1043 Triangulo

---
+++ 1043Triangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class 1043Triangulo
+{
+	public 1043Triangulo()
+	{
+
+        public 1043Triangulo(){
+        }
+
+		public void Triangulo()
+		{
+            string[] input = Console.ReadLine().Split(' ');
+
+            double a = Convert.ToDouble(input[0]);
+            double b = Convert.ToDouble(input[1]);
+            double c = Convert.ToDouble(input[2]);
+
+            if (a + b > c && b + c > a && a + c > b)
+            {
+                double perimetro = a + b + c;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("0.0"));
+            }
+            else
+            {
+                double trapezio = (a + b) * c / 2;
+                Console.WriteLine("Area = " + trapezio.ToString("0.0"));
+            }
+        }
+
+	}
+}
